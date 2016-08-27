208d0b811de2f6bd5247dd0ad6b1eaf7c945d907
Ashutosh Mishra <ashutoshmishra@Ashutoshs-MacBook-Air.local>
2016-08-27 14:57:15 -0400

    Modified plot.py such that plot is outputted as .png image instead of popup

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# Name: Earthquake Visualization
+# Description: An app that visualizes data from USGS using the matplotlib library
+# Author: Ashutosh Mishra
+
 import earthquakeData, getUserSelection, getURL
 #import plot
 
-# TODO add Tkinter GUI functionality (allow user to select which data they want to get from USGS)
 # Get relevent URL from: http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
 
 userChoice = getUserSelection.getUserSelection()
 
 url = getURL.getURL(userChoice)
-
 print(url)
 
 # Gets earthquake data from USGS
-#eqData = earthquakeData.getEqData(url)
+eqData = earthquakeData.getEqData(url)
+print(eqData)
+
 
 #plot.plotEq(eqData)
--- plot.py
+++ plot.py
@@ -8,21 +8,28 @@ def plotEq(data):
     for i in data:
         if i[1] != None:
             xVal.append(i[1])
-        else:
+        else:   #If i[1] == None
             xVal.append(0)
         if i[2] != None:
             yVal.append(i[2])
-        else:
+        else:   #If i[2] == None
             yVal.append(0)
 
-    #TODO label x/y axis and make graph look cleaner
-    #TODO allow user to interact with plot of earthquakes by clicking on data points (is this possible??)
-    #Formatting grapth
+    #TODO add labels indicating what time period and magnitude range is being plotted (e.g. past hour/1.0+)
+    #Plotting graph
     plt.plot(xVal, yVal, "r.")
-    plt.axis([int(min(xVal)), int(max(xVal))+1, 0, int(max(yVal))+1])
-    #print(int(max(yVal))+1)
+    #Formatting graph
+    plt.axis([min(xVal)-0.1, max(xVal)+0.1, -(1/20)*max(yVal),(21/20)*max(yVal)])
+    print(min(xVal))
+    print(max(xVal))
     plt.xlabel("Earthquake Magnitude")
     plt.ylabel("Number of Felt Reports")
     plt.title("Earthquake Felt Reports and Magnitude")
+    plt.grid()
 
-    plt.show()
+    #plt.show()
+    plt.savefig("plot")
+
+
+if __name__ == '__main__':
+    plotEq([['57km SSE of Chignik Lake, Alaska', 5, None], ['Southwest Indian Ridge', 4.8, None], ['86km S of Unalaska, Alaska', 4.5, None], ['49km NNE of Visokoi Island, South Georgia and the South Sandwich Islands', 5.4, None], ['Owen Fracture Zone region', 4.9, None], ['9km W of Sarahan, India', 4.6, 3], ['7km S of Sanchez, Dominican Republic', 4.5, 51]])
