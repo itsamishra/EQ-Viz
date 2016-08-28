ed50f423e6a4349725346fd238c2767ec9e0d0ae
Ashutosh Mishra <ashutoshmishra@Ashutoshs-MacBook-Air.local>
2016-08-28 19:38:40 -0400

    Converted earthquake data into list of earthquake objects

--- earthquakeData.py
+++ earthquakeData.py
@@ -7,8 +7,29 @@ class Earthquake:
         self.magnitude = 0
         self.feltReports = 0
         self.place = ""
+        self.time = 0
+        self.updated = 0.0
+        self.timezone = 0
+        self.url = ""
+        self.detail = ""
+        self.cdi = 0.0
+        self.mmi = 0.0
+        self.alert = ""
+        self.status= ""
+        self.tsunami = ""
+        self.sig = 0
+        self.net = ""
+        self.code = ""
+        self.ids = ""
+        self.sources = ""
+        self.type = ""
+        self.nst = 0
+        self.dmin = 0.0
+        self.rms = 0.0
+        self.gap = 0.0
+        self.mag = ""
+        self.type = ""
 
-    #def addEq(self):
     def addEq(self, JSONData, eqNum):
         # Setting id
         self.id = JSONData["features"][eqNum]["id"]
@@ -16,15 +37,31 @@ class Earthquake:
         for i in JSONData["features"][eqNum]["geometry"]["coordinates"]:
             self.location.append(i)
         # Add from "properties" portion of JSON
-        print(JSONData["features"][eqNum]["properties"])
         self.magnitude = JSONData["features"][eqNum]["properties"]["mag"]
-        if JSONData["features"][eqNum]["properties"]["felt"] != None:   #If it is none, feltReports remains zero
-            self.feltReports = JSONData["features"][eqNum]["properties"]["felt"]
+        self.feltReports = JSONData["features"][eqNum]["properties"]["felt"]
         self.place = JSONData["features"][eqNum]["properties"]["place"]
-
-
-
-        print("_____")
+        self.time = JSONData["features"][eqNum]["properties"]["time"]
+        self.updated = JSONData["features"][eqNum]["properties"]["updated"]
+        self.timezone = JSONData["features"][eqNum]["properties"]["tz"]
+        self.url = JSONData["features"][eqNum]["properties"]["url"]
+        self.detail = JSONData["features"][eqNum]["properties"]["detail"]
+        self.cdi = JSONData["features"][eqNum]["properties"]["cdi"]
+        self.mmi = JSONData["features"][eqNum]["properties"]["mmi"]
+        self.alert = JSONData["features"][eqNum]["properties"]["alert"]
+        self.status = JSONData["features"][eqNum]["properties"]["status"]
+        self.tsunami = JSONData["features"][eqNum]["properties"]["tsunami"]
+        self.sig = JSONData["features"][eqNum]["properties"]["sig"]
+        self.net = JSONData["features"][eqNum]["properties"]["net"]
+        self.code = JSONData["features"][eqNum]["properties"]["code"]
+        self.ids = JSONData["features"][eqNum]["properties"]["ids"]
+        self.sources = JSONData["features"][eqNum]["properties"]["sources"]
+        self.type = JSONData["features"][eqNum]["properties"]["type"]
+        self.nst = JSONData["features"][eqNum]["properties"]["nst"]
+        self.dmin = JSONData["features"][eqNum]["properties"]["dmin"]
+        self.rms = JSONData["features"][eqNum]["properties"]["rms"]
+        self.gap = JSONData["features"][eqNum]["properties"]["gap"]
+        self.mag = JSONData["features"][eqNum]["properties"]["mag"]
+        self.type = JSONData["features"][eqNum]["properties"]["type"]
 
 
 # TODO replace list with an Earthquake Data class
@@ -33,22 +70,20 @@ def getEqData(url):
     eqJSON = requests.get(url).json()
     numberOfEq = eqJSON["metadata"]["count"]
 
-    # Properties
-    #for j in range(len(eqJSON["features"])):
-    #    for i in eqJSON["features"][j]["properties"]:
-    #        print(eqJSON["features"][0]["properties"][i])
-
     for i in range(numberOfEq):
         earthquake = Earthquake()
         earthquake.addEq(eqJSON, i)
         eqData.append(earthquake)
 
+    ''' USE TO PRINT OUT CONTENTS OF ALL EARTHQUAKE OBJECTS
     for i in eqData:
-        print(i.id,i.location,i.magnitude,i.feltReports)
+        print(i.id, i.location, i.magnitude, i.feltReports, i.place, i.time, i.updated, i.timezone,
+          i.url, i.detail, i.cdi, i.mmi, i.alert, i.status, i.tsunami, i.sig, i.net,
+          i.code,
+          i.ids, i.sources, i.type, i.nst, i.dmin, i.rms, i.gap, i.mag, i.type)
+    '''
 
-#    sampleEq.addEq(eqJSON, numberOfEq)
-
-    return None
+    return eqData
 
 
 
